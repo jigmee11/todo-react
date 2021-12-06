92a1a713ef9bd5f0ff740a1aaeb7eae45ef75b1d
Tserenjigmed Shagdar <jigmee2006@gmail.com>
2021-12-06 13:48:04 +0800

    cool

---
+++ src/components/Todo.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
+import { RiCloseCircleLine } from 'react-icons/ri';
+import { TiEdit } from 'react-icons/ti';
+import { BiUpvote, BiDownvote } from 'react-icons/bi';
+
+const Todo = ({ todos, completeTodo, removeTodo, updateTodo, voted = false,vote=1,voteAll=10 }) => {
+  const [isVoted, setVoted] = useState(voted);
+  const [edit, setEdit] = useState({
+    id: null,
+    value: ''
+  });
+
+  const submitUpdate = value => {
+    updateTodo(edit.id, value);
+    setEdit({
+      id: null,
+      value: ''
+    });
+  };
+
+  if (edit.id) {
+    return <TodoForm edit={edit} onSubmit={submitUpdate} />;
+  }
+
+  return todos.map((todo, index) => (
+    <div
+      className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
+      key={index}
+    >
+      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+        {todo.text}
+      </div>
+      <div className='icons'>
+        <RiCloseCircleLine
+          onClick={() => removeTodo(todo.id)}
+          className='delete-icon'
+        />
+        <p id="vote">{vote}/{voteAll}</p>
+        {isVoted ?  <p>voted</p> : <BiUpvote className='delete-icon' />}
+        <TiEdit
+          onClick={() => setEdit({ id: todo.id, value: todo.text })}
+          className='edit-icon'
+        />
+      </div>
+    </div>
+  ));
+};
+
+export default Todo;
